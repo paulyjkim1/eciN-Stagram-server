1f37b15e4c3ae6aa8070f5f4e2233174a50c6d5c
paulyjkim1 <paulyjkim1@gmail.com>
2023-02-12 14:50:38 -0800

    add associations

--- models/post.js
+++ models/post.js
@@ -11,6 +11,8 @@ module.exports = (sequelize, DataTypes) => {
      */
     static associate(models) {
       // define association here
+      models.post.hasMany(models.comment)
+      models.post.belongsTo(models.user)
     }
   }
   post.init({
